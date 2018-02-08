6052c6a9e3b2b2d418a518462d1ef88c8f73f987
masktaq <50889574@qq.com>
2018-02-08 14:37:59 +0800

    完成设备管理页面逻辑

--- src/pages/device-manage/index.js
+++ src/pages/device-manage/index.js
@@ -1,34 +1,70 @@
 import React, { Component } from "react";
-import { View, Text, FlatList } from "react-native";
-//import PropTypes from "prop-types";
+import { View, Text, FlatList, TouchableOpacity, Image } from "react-native";
+import PropTypes from "prop-types";
 
-import { Page, Button } from "src/components";
+import { Page, Button, Input } from "src/components";
 import styles from "./style";
+
+const CheckBox = ({ checked, onChangeChecked }) => (
+  <TouchableOpacity
+    onPress={() => {
+      onChangeChecked(!checked);
+    }}
+    style={styles.checkbox}
+  >
+    <Image
+      style={styles.checkboxImg}
+      source={checked ? require() : require()}
+    />
+  </TouchableOpacity>
+);
+CheckBox.propTypes = {
+  checked: PropTypes.bool,
+  onChangeChecked: PropTypes.func
+};
 export default class DeviceManage extends Component {
   static defaultProps = {};
   static propTypes = {};
-  state = {};
-  renderItem(item) {
-    const { label, value } = item;
+  state = {
+    data: [
+      { label: "淋浴", value: "", checked: true },
+      { label: "储物", value: "", checked: true },
+      { label: "有氧器材", value: "11", checked: true },
+      { label: "力量器材", value: "12", checked: true },
+      { label: "康体设备", value: "13", checked: true }
+    ]
+  };
+  handleValueChange(v, i, key) {
+    const nextData = Object.assign([], this.state.data);
+    nextData[i][key] = v;
+    this.setState({
+      data: nextData
+    });
+  }
+
+  renderItem = (item, i) => {
+    const { label, value, checked } = item;
     return (
       <View style={styles.item} key={label}>
         <View style={styles.itemLeft}>
           <Text style={styles.itemLabel}>{label}</Text>
         </View>
         <View style={styles.itemRight}>
-          <Text style={styles.itemValue}>{value}</Text>
+          <Input
+            value={value}
+            onChangeText={v => this.handleValueChange(v, i, "value")}
+            style={styles.itemValue}
+          />
+          <CheckBox
+            checked={checked}
+            onChangeChecked={v => this.handleValueChange(v, i, "checked")}
+          />
         </View>
       </View>
     );
-  }
+  };
   render() {
-    const data = [
-      { label: "淋浴", value: "" },
-      { label: "储物", value: "" },
-      { label: "有氧器材", value: "11" },
-      { label: "力量器材", value: "12" },
-      { label: "康体设备", value: "13" }
-    ];
+    const { data } = this.state;
     return (
       <Page title="设备管理">
         <View style={styles.container}>
@@ -36,7 +72,7 @@ export default class DeviceManage extends Component {
             data={data}
             keyExtractor={(row, i) => i}
             ItemSeparatorComponent={() => <View style={styles.itemBorder} />}
-            renderItem={({ item }) => this.renderItem(item)}
+            renderItem={({ item, index }) => this.renderItem(item, index)}
           />
           <Button
             onPress={this.saveRow}
